960874db9ee8d3e0e35ea0987d0c91ea2fb2b87e
isirmacokaciyo <94755848+isirmacokaciyo@users.noreply.github.com>
2021-12-21 07:21:16 +0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 mr-6 bg-indigo-100">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-indigo-100">
       <title>Napim</title>
-        <h1 className="text-center bg-clip-text text-transparent text-6xl font-bold bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 pl-96 animate-pulse">
+        <h1 className="text-center bg-clip-text text-transparent text-6xl font-bold bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 animate-pulse">
             NAPIM
         </h1>
     </div>
