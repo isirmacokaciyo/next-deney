f4e47ccee3fa4bf7dbe90c290c5bd67c67e6a710
isirmacokaciyo <94755848+isirmacokaciyo@users.noreply.github.com>
2022-01-10 18:05:43 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-indigo-100">
-      <title>Napim</title>
+      <title>EMRE GAYMIŞ</title>
         <h1 className="text-center bg-clip-text text-transparent text-6xl font-bold bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 animate-pulse">
-            NAPIM
+            EMRE GAYMIŞ
         </h1>
     </div>
   )
